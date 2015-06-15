db6d1130ef0b8f6666fb1c445154fa64fda99fac
Nicholas Wood <woodne@gmail.com>
2015-06-14 21:43:47 -0400

    defeated navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,7 @@ var React = require( 'react' );
 var RsvpForm = require( './rsvp-form.js' );
 
 React.render( <RsvpForm />, document.getElementById( 'rsvp-form' ) );
+
+$('.nav a').on('click', function(){
+    $(".navbar-toggle").click() //bootstrap 3.x by Richard
+});
