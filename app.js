ea65a1f02259f7f7ef98dcc04e1cd5d0916f4526
Nicholas Wood <woodne@gmail.com>
2015-07-01 19:43:15 -0400

    removing logging

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var url = require( 'url' );
@@ -18,7 +17,7 @@ var Promise = require( 'bluebird' );
 // app.use(favicon(__dirname + '/public/favicon.ico'));
 
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
